e2c2719163e189ce20411bb3565c0f1af8f714b3
JoonasEronen <joonas@joonaseronen.com>
2026-02-01 18:09:09 +0200

    Add IAM role/policy for Lambda (least privilege DynamoDB + logs)

---
+++ infra/iam.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${local.name_prefix}-lambda-exec"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "dynamodb_write" {
+  name = "${local.name_prefix}-dynamodb-write"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:PutItem"
+      ]
+      Resource = aws_dynamodb_table.events.arn
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.dynamodb_write.arn
+}
