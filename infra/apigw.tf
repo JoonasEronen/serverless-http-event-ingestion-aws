e37754c56829a959b603dd88b9509fef66af95d2
JoonasEronen <joonas@joonaseronen.com>
2026-02-09 00:13:43 +0200

    Project 1: working serverless ingestion (API Gateway + Lambda + DynamoDB)

---
+++ infra/apigw.tf
@@ -0,0 +1,38 @@
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "${local.name_prefix}-http-api"
+  protocol_type = "HTTP"
+  tags          = local.tags
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.ingestion.arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "post_events" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /events"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+  tags        = local.tags
+}
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ingestion.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
---
+++ infra/lambda.tf
@@ -0,0 +1,25 @@
+resource "aws_lambda_function" "ingestion" {
+  function_name = "${local.name_prefix}-event-ingestion"
+  role          = aws_iam_role.lambda_exec.arn
+
+  runtime = "python3.12"
+  handler = "lambda_handler.lambda_handler"
+
+  filename         = "${path.module}/../build/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../build/lambda.zip")
+
+  timeout     = 10
+  memory_size = 128
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.events.name
+    }
+  }
+
+  tags = local.tags
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.ingestion.function_name
+}
